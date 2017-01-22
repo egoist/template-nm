8290a6548447cf9a3ec1a3b69fcf56872f206f6f
EGOIST <0x142857@gmail.com>
2017-01-22 20:25:16 +0800

    fix test file

--- template/__test__/index.test.js
+++ template/__test__/index.test.js
@@ -1,5 +1,4 @@
-import test from 'ava'
-import <%= this.camelcase(name) %> from '../'
+const <%= this.camelcase(name) %> = require('../')
 
 test('main', () => {
   expect(typeof <%= this.camelcase(name) %>).toBe('function')
