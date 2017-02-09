007795c70deb4f4ad31e1b3aebfc88a5480a0bdf
zcong1993 <1005997919@qq.com>
2017-02-09 15:00:45 +0800

    add xo.envs jest config for jest and make init template can pass xo lint

--- template/index.js
+++ template/index.js
@@ -2,4 +2,5 @@ module.exports = function (input, options = {}) {
   if (typeof input !== 'string') {
     throw new Error('Expected input to be string')
   }
+  return Object.assign(options, {input})
 }
