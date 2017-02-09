cb22747d2a0670ec7b5fd48720d63555ce5b8b56
EGOIST <0x142857@gmail.com>
2017-02-09 15:22:16 +0800

    Use more meaningful example

--- template/index.js
+++ template/index.js
@@ -2,5 +2,6 @@ module.exports = function (input, options = {}) {
   if (typeof input !== 'string') {
     throw new Error('Expected input to be string')
   }
-  return Object.assign(options, {input})
+  
+  return `${input} & ${options.postfix || 'rainbow'}`
 }
