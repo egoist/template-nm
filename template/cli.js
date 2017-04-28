c0c494776020efa40d22c1101b5a11baa793a5ee
EGOIST <0x142857@gmail.com>
2017-04-28 18:06:58 +0800

    tweak cli

--- template/cli.js
+++ template/cli.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 'use strict'
+const yargs = require('yargs')
+const pkg = require('./package')
 const main = require('./')
-const argv = require('yargs').argv
+
+const argv = yargs
+  .version(pkg.version)
+  .help()
+  .argv
 
 main(argv)
