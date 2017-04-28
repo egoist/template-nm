ecba687ebc6783f77c7d06ddf650f1c328d52fe2
EGOIST <0x142857@gmail.com>
2017-04-28 18:00:40 +0800

    tweaks

--- template/index.js
+++ template/index.js
@@ -1,6 +1,6 @@
 module.exports = function (input, options = {}) {
   if (typeof input !== 'string') {
-    throw new Error('Expected input to be string')
+    throw new TypeError('Expected input to be string')
   }
 
   return `${input} & ${options.postfix || 'rainbow'}`
